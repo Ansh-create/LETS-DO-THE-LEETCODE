7d893f62e684d50af5a3ce73a6c74c83a35a68b0
ANSH GUPTA <65327006+Ansh-create@users.noreply.github.com>
2022-06-04 09:28:45 +0530

    Time: 492 ms (71.78%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT(product_id) , 'store1' as store, store1 as price FROM Products WHERE store1 is not NULL
+
+UNION
+
+SELECT DISTINCT(product_id) , 'store2' as store, store2 as price FROM Products WHERE store2 is not NULL
+
+UNION
+
+SELECT DISTINCT(product_id), 'store3' as store, store3 as price FROM Products WHERE store3 is not NULL;
